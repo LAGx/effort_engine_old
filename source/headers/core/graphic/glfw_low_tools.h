65c82e7289ff5cc70ad9efa9c6f16b7e65b0830f
LAGx <KVARKmail@gmail.com>
2018-03-04 00:36:20 +0200

    divide vulkan manager by hikka classes

---
+++ source/headers/core/graphic/glfw_low_tools.h
@@ -0,0 +1,32 @@
+
+
+
+
+
+/*! 
+    @brief namespace of engine
+*/
+namespace eff{
+/*! 
+    @brief namespace for engine developers
+    @warning include complex tool, that have to work in complex configuration
+    @warning don`t use as part of user interface engine
+*/
+namespace eff_low{
+
+
+class Window{
+public:
+
+    struct CreateInfo{
+
+    };
+
+protected:
+
+};
+
+
+
+};
+};
---
+++ source/headers/core/graphic/vulkan_low_tools.h
@@ -0,0 +1,86 @@
+
+
+
+
+
+/*! 
+    @brief namespace of engine
+*/
+namespace eff{
+/*! 
+    @brief namespace for engine developers
+    @warning include complex tool, that have to work in complex configuration
+    @warning don`t use as part of user interface engine
+*/
+namespace eff_low{
+
+
+
+class Surface{
+public:
+
+protected:
+
+};
+
+
+
+class Instance{
+public:
+
+    struct CreateInfo{
+
+    };
+
+protected:
+
+};
+
+
+
+class ValidationLayers{
+public:
+
+protected:
+
+};
+
+
+
+class PhysicalDevice{
+public:
+
+protected:
+
+};
+
+
+
+class LogicalDevice{
+public:
+
+protected:
+
+};
+
+
+
+class QueueFamilyes{
+public:
+
+protected:
+
+};
+
+
+
+
+
+
+
+
+
+
+
+};
+};
